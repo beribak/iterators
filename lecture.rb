f427bc508716731c772569ee4f03a344e5583057
Danko Beribak <beribakdanko@gmail.com>
2022-01-13 23:40:53 +0100

    refractor lecture notes

--- lecture.rb
+++ lecture.rb
@@ -1,18 +1,22 @@
-array = ["Danko afasd", "Ofer adasdasdas", "Francesca adasddsad", "Thomas adfasdsadsa", "123", "123"]
 
-array = array.each do |name|
-    # puts name.upcase
-end 
+# ITERATOR METHODS
 
+array = ["Danko afasd", "Ofer adasdasdas", "Francesca adasddsad", "Thomas adfasdsadsa", "Ema", "Iri"]
 
+# EACH
+# array = array.each do |name|
+#     puts name.upcase
+# end 
+
+# EACH_WITH_INDEX
 # variable = array.each_with_index do |name, index|
 #     puts name.upcase
 #     puts index
 # end
 
+# MAP
 # array = array.map do |name|
 #     name.upcase
-# end 
 # end 
 
 # array = array.map do |name|
@@ -20,16 +24,19 @@ end
 #     name.split(" ").first
 # end
 
+# COUNT
 # number = array.count do |name|
 #     name.length < 5
 # end
 
+# SELECT
 # new_array = array.select do |name|
 #     name.length < 5
 # end
 
 # sleep(5) 
 
+# YIELD
 # def timer
 #     time = Time.now
 #     p "Welcome to the timer method"
@@ -43,7 +50,7 @@ end
 # def timer(first_name, last_name)
 #     time = Time.now
 #     p "Welcome to the timer method"
-   
+
 #     name = "#{first_name} #{last_name}"
    
 #     yield(name)
@@ -51,29 +58,28 @@ end
 #     puts time
 # end
 
-# timer("Danko", "Beribak") do |name|
+# timer("Danko", "Beribak") do |name|  ##==> THIS IS A METHOD CALL. DO NOT UNCOMMENT!!!
 #     puts "hello #{name}"
 # end
 
-# def custom_each(array)
+# IMITATE THE EACH METHOD. THIS METHOD DOES THE SAME THING AS EACH
+# def custom_each(array) 
 #     for item in array
 #         yield(item)
 #     end
 # end
 
-# custom_each(array) do |item|
+# custom_each(array) do |item| ##==> THIS IS A METHOD CALL. DO NOT UNCOMMENT!!!
 #     p item
 # end
-# p new_array
 
-def insert_joke
-    puts "Here is a greate joke:"
-    yield("whats up i am coming from the method")
-    puts "hahahaha"
-    yield("danko")
-end
+# def tell_me_a_joke
+#     puts "Here is a greate joke:"
+#     yield("Knock! Knock! Who is it? The KGB!!!")
+#     puts "hahahaha"
+#     yield("it is me again")
+# end
 
-insert_joke do |sentence|
-    puts sentence
-    # puts joke
-end
+# tell_me_a_joke do |joke| ##==> THIS IS A METHOD CALL. DO NOT UNCOMMENT!!!
+#     puts joke
+# end
--- livecode.rb
+++ livecode.rb
@@ -5,7 +5,7 @@ def louchebemize(sentence)
     word = sentence.split(" ")[0] # get the first word of the sentence passed to this method
     # 2. if the word containes one letter then do nothing   
     if word.length != 1
-    # 3 a). see if the word starts with a vowel or a consonant
+       # 3 a). see if the word starts with a vowel or a consonant
       if !word.start_with?('a', 'e', 'i', 'u', 'o') # or unless word.start_with?('a', 'e', 'i', 'u', 'o')
         # 3. a) If the word starts wit a consonant get all the consonantes before the first vowel 
                 #and put the at the end of the word
@@ -13,7 +13,7 @@ def louchebemize(sentence)
           if "aeiou".include?(letter) # check if the current letter in the itarration is a vawol
             position = word.index(letter) # get the index of the first vowel in the word 
             word = word[position..-1] + word[0...position] # get the sections of the string separated by the first vowel 
-                                                            # and switch their places 
+                                                           # and switch their places 
             break 
           end
         end
@@ -23,7 +23,6 @@ def louchebemize(sentence)
       # 4. b) If the words beginning with a vowel are not changed but you should still add an l to the start of the word 
       #       and a suffix at the end (“atout” should give “latoutoc” or “latoutic”)
       word = "l#{word}#{sufixes.sample}"
-    
     end
   
     return word
