1b5b08c1522edd66b10d8caa389b95ba27cb8314
Danko Beribak <beribakdanko@gmail.com>
2022-01-13 22:33:23 +0100

    refractoring

--- livecode.rb
+++ livecode.rb
@@ -2,16 +2,16 @@
 def louchebemize(sentence)
     sufixes = ["em", "é", "ji", "oc", "ic", "uche", "ès"] # sufixes
     # 1. get the first word of the sentence
-    word = sentence.split(" ")[0]
+    word = sentence.split(" ")[0] # get the first word of the sentence passed to this method
     # 2. if the word containes one letter then do nothing   
     if word.length != 1
     # 3 a). see if the word starts with a vowel or a consonant
-      if !word.start_with?('a', 'e', 'i', 'u', 'o')
+      if !word.start_with?('a', 'e', 'i', 'u', 'o') # or unless word.start_with?('a', 'e', 'i', 'u', 'o')
         # 3. a) If the word starts wit a consonant get all the consonantes before the first vowel 
                 #and put the at the end of the word
         word.chars.each do |letter|
           if "aeiou".include?(letter) # check if the current letter in the itarration is a vawol
-            position = word.index(letter) # get the index of the letter in the word 
+            position = word.index(letter) # get the index of the first vowel in the word 
             word = word[position..-1] + word[0...position] # get the sections of the string separated by the first vowel 
                                                             # and switch their places 
             break 
@@ -28,7 +28,7 @@ def louchebemize(sentence)
   
     return word
   end
-  
+
   #use this for testing
   #puts louchebemize("Hello World!!!") 
   
